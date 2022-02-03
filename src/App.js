cca038ba50546bad9931e2b90c97c1e5664dff1d
Your Name <you@example.com>
2022-02-03 14:40:37 +0200

    Commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,13 @@
 import logo from './logo.svg';
-import './App.css';
+import './styles.css';
+import SocialNetwork from './SocialNetwork';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Separate Hooks for Separate Effects</h1>
+        <SocialNetwork />
       </header>
     </div>
   );
